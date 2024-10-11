2ad91c33435aec72ddd8ae47211737cea74c0f7b
Evgeniy Olkhin <mrjeka111@gmail.com>
2024-10-11 12:49:17 +0500

    Change database initialization and configuration for able writing tests with another database

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,118 +1,148 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "mydatabase.db", // Путь к файлу базы данных SQLite
-  define: {
-    timestamps: false, // Отключить создание полей createdAt и updatedAt
-  },
-});
-// Определение модели "Person"
-const Zvanie = sequelize.define("Zvanie", {
-  name: DataTypes.STRING,
-});
+const database_init = (dialect, storage) => {
+  const sequelize = new Sequelize({
+    dialect: dialect, // Выбор диалекта
+    storage: storage, // Путь к файлу базы данных SQLite
+    define: {
+      timestamps: false, // Отключить создание полей createdAt и updatedAt
+    },
+  });
+  // Определение модели "Person"
+  const Zvanie = sequelize.define("Zvanie", {
+    name: DataTypes.STRING,
+  });
 
-const Podrazdelenie = sequelize.define("Podrazdelenie", {
-  name: DataTypes.STRING,
-});
+  const Podrazdelenie = sequelize.define("Podrazdelenie", {
+    name: DataTypes.STRING,
+  });
 
-const Category = sequelize.define("Category", {
-  name: DataTypes.STRING,
-  shortName: DataTypes.STRING,
-});
-const UprazhnenieRealValuesType = sequelize.define(
-  "UprazhnenieRealValuesType",
-  {
+  const Category = sequelize.define("Category", {
     name: DataTypes.STRING,
     shortName: DataTypes.STRING,
+  });
+  const UprazhnenieRealValuesType = sequelize.define(
+    "UprazhnenieRealValuesType",
+    {
+      name: DataTypes.STRING,
+      shortName: DataTypes.STRING,
+    }
+  );
+
+  // Определение модели "Person"
+  const Person = sequelize.define("Person", {
+    fName: DataTypes.STRING,
+    lName: DataTypes.STRING,
+    sName: DataTypes.STRING,
+    dob: DataTypes.DATE,
+    comment: DataTypes.STRING,
+    num: DataTypes.STRING,
+    isMale: DataTypes.BOOLEAN,
+    isV: DataTypes.BOOLEAN,
+    rating: DataTypes.INTEGER,
+    isFree: DataTypes.BOOLEAN,
+    otpuskFrom: DataTypes.DATE,
+    otpuskTo: DataTypes.DATE,
+  });
+
+  const EfficiencyPreference = sequelize.define("EfficiencyPreference", {
+    name: DataTypes.STRING,
+  });
+
+  const Uprazhnenie = sequelize.define("Uprazhnenie", {
+    name: DataTypes.STRING,
+    shortName: DataTypes.STRING,
+    uprazhnenieRealValuesTypeId: UprazhnenieRealValuesType,
+    step: DataTypes.DOUBLE,
+    valueToAddAfterMaxResult: DataTypes.INTEGER,
+  });
+
+  const UprazhnenieStandard = sequelize.define("UprazhnenieStandard", {
+    // uprazhnenieId: Uprazhnenie,
+    categoryId: Category,
+    value: DataTypes.DOUBLE,
+    result: DataTypes.INTEGER,
+  });
+
+  const UprazhnenieSchedule = sequelize.define("UprazhnenieSchedule", {
+    uprazhnenieId: Uprazhnenie,
+    name: Category,
+    personId: Person,
+    date: DataTypes.DATE,
+  });
+
+  const UprazhnenieResult = sequelize.define("UprazhnenieResult", {
+    result: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+  });
+  Person.hasMany(UprazhnenieResult);
+  UprazhnenieResult.belongsTo(Person);
+  EfficiencyPreference.hasMany(Uprazhnenie);
+  Uprazhnenie.belongsTo(EfficiencyPreference);
+  Zvanie.hasMany(Person, {
+    foreignKey: "zvanieId",
+  });
+  Person.belongsTo(Zvanie, {
+    foreignKey: "zvanieId",
+  });
+
+  Podrazdelenie.hasMany(Person, {
+    foreignKey: "podrazdelenieId",
+  });
+  Person.belongsTo(Podrazdelenie, {
+    foreignKey: "podrazdelenieId",
+  });
+
+  Uprazhnenie.hasMany(UprazhnenieStandard, {
+    foreignKey: "uprazhnenieId",
+  });
+  UprazhnenieStandard.belongsTo(Uprazhnenie, {
+    foreignKey: "uprazhnenieId",
+  });
+
+  Category.hasMany(Person, {
+    foreignKey: "categoryId",
+  });
+  Person.belongsTo(Category, {
+    foreignKey: "categoryId",
+  });
+  Category.hasMany(UprazhnenieResult);
+  UprazhnenieResult.belongsTo(Category);
+  Uprazhnenie.hasMany(UprazhnenieResult);
+  UprazhnenieResult.belongsTo(Uprazhnenie);
+
+  return {
+    UprazhnenieResult: UprazhnenieResult,
+    UprazhnenieSchedule: UprazhnenieSchedule,
+    sequelize: sequelize,
+    UprazhnenieStandard: UprazhnenieStandard,
+    Person: Person,
+    Zvanie: Zvanie,
+    Podrazdelenie: Podrazdelenie,
+    Category: Category,
+    Uprazhnenie: Uprazhnenie,
+    UprazhnenieRealValuesType: UprazhnenieRealValuesType,
+    EfficiencyPreference: EfficiencyPreference,
   }
-);
+};
 
-// Определение модели "Person"
-const Person = sequelize.define("Person", {
-  fName: DataTypes.STRING,
-  lName: DataTypes.STRING,
-  sName: DataTypes.STRING,
-  dob: DataTypes.DATE,
-  comment: DataTypes.STRING,
-  num: DataTypes.STRING,
-  isMale: DataTypes.BOOLEAN,
-  isV: DataTypes.BOOLEAN,
-  rating: DataTypes.INTEGER,
-  isFree: DataTypes.BOOLEAN,
-  otpuskFrom: DataTypes.DATE,
-  otpuskTo: DataTypes.DATE,
-});
-
-const EfficiencyPreference = sequelize.define("EfficiencyPreference", {
-  name: DataTypes.STRING,
-});
-
-const Uprazhnenie = sequelize.define("Uprazhnenie", {
-  name: DataTypes.STRING,
-  shortName: DataTypes.STRING,
-  uprazhnenieRealValuesTypeId: UprazhnenieRealValuesType,
-  step: DataTypes.DOUBLE,
-  valueToAddAfterMaxResult: DataTypes.INTEGER,
-});
-
-const UprazhnenieStandard = sequelize.define("UprazhnenieStandard", {
-  // uprazhnenieId: Uprazhnenie,
-  categoryId: Category,
-  value: DataTypes.DOUBLE,
-  result: DataTypes.INTEGER,
-});
-
-const UprazhnenieSchedule = sequelize.define("UprazhnenieSchedule", {
-  uprazhnenieId: Uprazhnenie,
-  name: Category,
-  personId: Person,
-  date: DataTypes.DATE,
-});
-
-const UprazhnenieResult = sequelize.define("UprazhnenieResult", {
-  result: DataTypes.INTEGER,
-  date: DataTypes.DATE,
-});
-Person.hasMany(UprazhnenieResult);
-UprazhnenieResult.belongsTo(Person);
-EfficiencyPreference.hasMany(Uprazhnenie);
-Uprazhnenie.belongsTo(EfficiencyPreference);
-Zvanie.hasMany(Person, {
-  foreignKey: "zvanieId",
-});
-Person.belongsTo(Zvanie, {
-  foreignKey: "zvanieId",
-});
-
-Podrazdelenie.hasMany(Person, {
-  foreignKey: "podrazdelenieId",
-});
-Person.belongsTo(Podrazdelenie, {
-  foreignKey: "podrazdelenieId",
-});
-
-Uprazhnenie.hasMany(UprazhnenieStandard, {
-  foreignKey: "uprazhnenieId",
-});
-UprazhnenieStandard.belongsTo(Uprazhnenie, {
-  foreignKey: "uprazhnenieId",
-});
-
-Category.hasMany(Person, {
-  foreignKey: "categoryId",
-});
-Person.belongsTo(Category, {
-  foreignKey: "categoryId",
-});
-Category.hasMany(UprazhnenieResult);
-UprazhnenieResult.belongsTo(Category);
-Uprazhnenie.hasMany(UprazhnenieResult);
-UprazhnenieResult.belongsTo(Uprazhnenie);
+const dbDefinition = database_init("sqlite", "mydatabase.db");
 
 
+const UprazhnenieResult = dbDefinition.UprazhnenieResult
+const UprazhnenieSchedule = dbDefinition.UprazhnenieSchedule
+const sequelize = dbDefinition.sequelize
+const UprazhnenieStandard = dbDefinition.UprazhnenieStandard
+const Person = dbDefinition.Person
+const Zvanie = dbDefinition.Zvanie
+const Podrazdelenie = dbDefinition.Podrazdelenie
+const Category = dbDefinition.Category
+const Uprazhnenie = dbDefinition.Uprazhnenie
+const UprazhnenieRealValuesType = dbDefinition.UprazhnenieRealValuesType
+const EfficiencyPreference = dbDefinition.EfficiencyPreference
 
 module.exports = {
+  database_init,
   UprazhnenieResult,
   UprazhnenieSchedule,
   sequelize,
