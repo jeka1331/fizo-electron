998584db468d6d4c6e6664a88c4e46c173c7b160
Axe <azarkural2001@gmail.com>
2024-11-05 13:45:23 +0500

    Fix running sidecar from container

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,30 +1,40 @@
+use std::env;
 use std::process::Command;
 use std::thread;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  // Spawn a new thread to run the other application
-  thread::spawn(|| {
-    let output = Command::new("./server")
-      .output()
-      .expect("failed to execute process");
+    // Spawn a new thread to run the other application
+    thread::spawn(|| {
+        let path_exe = env::current_exe().expect("failed to get path of exe");
+        let path_dir = path_exe.parent().expect("failet to get dir path");
+        let path_server = format!(
+            "{}",
+            if cfg!(target_os = "windows") {
+                format!("{}\\server.exe", path_dir.display())
+            } else {
+                format!("{}/server", path_dir.display())
+            }
+        );
+        println!("{}", path_server);
+        Command::new(path_server)
+            .spawn()
+            .expect("failed to execute process");
 
-    if !output.status.success() {
-      eprintln!("Application exited with error: {:?}", output);
-    }
-  });
 
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    });
+
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
